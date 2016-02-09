3af22dc04cb44af935cc3804fa90ca723dce58ef
Alvaro Tello <alvarosebastian.tello@gmail.com>
2016-02-09 17:11:23 -0300

    Modificacion y Creacion pagina para nuevo articulo
    Se modifico el Menu para una futura reestructuracion de la pagina
    inicial, ademas de crear una pagina de conexion a mongodb y asi
    almacenar los nuevos articulos que se fueran a ingresar.

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 // NodeJS Document
+// Declaración de Variables
 var express = require('express');
-//var mongoose = require('mongoose');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+//Variable que hago utilizar body-parser para poder capturar los datos
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+//Variable que señala donde esta ubicada la base de datos (mongodb://ipdelservidor/basededatosausar)
+mongoose.connect("mongodb://localhost/desarmaduria");
+
+//Definir el schema de nuestros productos
+var productSchema = {
+	title:String,
+	year:Number,
+	klms:Number,
+	price:Number,
+	description:String
+};
+
+var Product = mongoose.model("Product", productSchema);
 
 var app = express();
 
@@ -11,12 +31,40 @@ app.set("view engine","jade");
 app.use(express.static("public"));
 
 app.get("/",function(req,res){
-
 	//Menciono la pagina que yo quiero renderizar
 	res.render("index");
 	//Comando para finalizar solicitud de el navegador y nos permite enviar solo una cadena
 	//res.end("Hola Mundo");
 });
 
+//Recibe y procesa la informacion que recibire del formulario
+app.post("/product",function(req,res){
+	console.log(req.body);
+
+	var data = {
+		title=req.body.title,
+		year=req.body.year,
+		klms=req.body.klms,
+		price=req.body.price,
+		description=req.body.description
+	}
+
+	var product = new Product(data);
+
+	product.save(function(err){
+		console.log(product);
+		res.render("index");
+	});
+
+	res.render("product/new");
+});
+
+//Referencia a una nueva vista dentro de otra carpeta
+app.get("/product/new",function(req,res){
+	//Menciono la pagina que yo quiero renderizar
+	res.render("product/new");
+	//Comando para finalizar solicitud de el navegador y nos permite enviar solo una cadena
+	//res.end("Agregar Nueva Funcion");
+});
 
 app.listen(8080);
